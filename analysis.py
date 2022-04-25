30f2b6a4cfd8da9d4d93089d1e80099620dfc993
SaydsC <G00411297@gmit.ie>
2022-04-25 12:46:46 +0100

    seaborn

--- analysis.py
+++ analysis.py
@@ -5,6 +5,7 @@
 #performs any other analysis you think is appropriate
 
 #import libraries to work with the dataset iris.csv,
+import matplotlib
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt #for plotting
@@ -27,5 +28,9 @@ import seaborn as sns
 f = plt.figure(figsize=(11,6))
 fig = sns.scatterplot(x="sepal.length", y="petal.length", data=data)
 plt.show()
+
+sns.set()
+
+
 
 
