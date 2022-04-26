5d52b76b60095e1958026e9381a224ce186c68df
SaydsC <G00411297@gmit.ie>
2022-04-26 14:41:11 +0100

    txt

--- analysis.py
+++ analysis.py
@@ -8,9 +8,10 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt #for plotting
+import sys
 
-file = 'summary.txt'
-f = open(file, "w")
+sys.stdout = open ("summary.txt","w")
+print ("\n")
 
 data = pd.read_csv("iris.csv")
 print (data.head(150))
